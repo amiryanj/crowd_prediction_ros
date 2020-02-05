039c170442a51596265001b62db58d236703eb06
Javad <javad.amirian@inria.fr>
2020-02-05 10:59:21 -0600

    first implementation of pcf synthesis

--- src/crowd_prediction/crowd_synthesis/crowd_synthesis.py
+++ src/crowd_prediction/crowd_synthesis/crowd_synthesis.py
@@ -1,6 +1,8 @@
 import os
+import random
+
 import numpy as np
-from crowd_prediction.crowd_synthesis.pcf import pcf
+from crowd_prediction.crowd_synthesis.pcf import PcfPattern, DartThrowing
 from sklearn.mixture import gaussian_mixture
 from crowd_prediction.opentraj_tools.parser.parser_eth import ParserETH
 from sklearn import cluster, datasets, mixture
@@ -14,52 +16,64 @@ class CrowdSynthesizer:
         self.pcf_range = np.arange(0.2, 10, 0.05)
         self.grid_size = (64, 64)
         self.vel_gmm_n_components = 4
-        self.pcf_map = np.zeros(self.pcf_range.size)
-        self.heatmap_grid = np.zeros(self.grid_size, dtype=np.float)
+        self.target_pcf = PcfPattern()
+        self.heatmap_grid = np.ones(self.grid_size, dtype=np.float)
         self.vel_gmm_grid = [[mixture.GaussianMixture(n_components=self.vel_gmm_n_components, covariance_type='tied', max_iter=100)
-                               for _ in range(self.grid_size[1])]
-                                for j in range(self.grid_size[0])]
+                              for _ in range(self.grid_size[1])]
+                             for j in range(self.grid_size[0])]
 
-        self.synthesis_stop_n_try = 500
+        self.synthesis_max_try = 500
+        self.synthesis_max_pts = 10  # FIXME: calc from data
+        self.dart_thrower = DartThrowing()
 
     # ===================================
     # ======== Extract Features =========
     # ===================================
     def extract_features(self, dataset):
-        self.pcf_map = self.compute_avg_pcf(dataset)
-        self.heatmap_grid = self.compute_heatmap(dataset)
-        self.vel_gmm_grid = self.compute_vel_gmm(dataset)
+        self.compute_target_pcf(dataset)
+        self.compute_heatmap(dataset)
+        # self.compute_vel_gmm(dataset)
 
-    def compute_avg_pcf(self, dataset):
+    def compute_target_pcf(self, dataset):
         all_pcfs = []
         for t in dataset.t_p_dict:
-            pcf_t = pcf(dataset.t_p_data[t])
-            all_pcfs.append(pcf_t)
-        avg_pcf = np.array(all_pcfs).mean(axis=1)
-        return avg_pcf
+            self.target_pcf.update(dataset.t_p_dict[t])
+            pcf_t = self.target_pcf.pcf_values
+            for ii, r in enumerate(self.target_pcf.rad_values):
+                all_pcfs.append(pcf_t)
+        all_pcfs = np.array(all_pcfs)
+        print('*****************', all_pcfs[0].shape)
 
     def compute_heatmap(self, dataset):
         all_points = dataset.get_all_points()
-
         all_points = np.array(all_points)
-        heatmap_grid = np.histogram2d(all_points[:, 0], all_points[:, 1], bins=50)
-        return heatmap_grid
 
+        # heatmap should be smoothed
+        self.heatmap_grid, xedges, yedges  = np.histogram2d(all_points[:, 0], all_points[:, 1], bins=self.grid_size)
+
+        x_min, x_max = min(all_points[:, 0]), max(all_points[:, 0])
+        y_min, y_max = min(all_points[:, 1]), max(all_points[:, 1])
+        # FIXME: grid_size[0] and grid_size[1] may need to exchange thier place
+        map_to_world_coord = lambda ind: [(ind %  self.grid_size[1] +0.5) / self.grid_size[0] * (x_max - x_min) + x_min,
+                                          (ind // self.grid_size[1] +0.5) / self.grid_size[1] * (y_max - y_min) + y_min]
+        self.dart_thrower.set_pdf_grid(self.heatmap_grid, map_to_world_coord)
 
     def compute_vel_gmm(self, dataset):
         all_trajs = dataset.get_all_trajs()
-        all_pv =
-        pass
+        all_vs = [traj_i[1:] - traj_i[:-1] for traj_i in all_trajs]
+        all_pvs = []
+        for ii in range(len(all_trajs)):
+            all_pvs.extend(np.stack(all_trajs[ii][:-1], all_vs[ii]))
+        self.vel_gmm_grid = np.zeros(self.grid_size, dtype=float)
 
     # ===================================
     # ======== Synthesize Crowd =========
     # ===================================
     def synthesize_init(self, detections):
-        temp_points = []
         final_points = detections.copy()
 
         try_counter = 0
-        while try_counter < self.synthesis_stop_n_try:
+        while try_counter < self.synthesis_max_try:
             try_counter += 1
 
             temp_points = final_points.copy()
@@ -67,22 +81,28 @@ class CrowdSynthesizer:
             temp_points.append(p_new)
             if self.check_pcf_error(temp_points):
                 final_points.append(p_new)
+            if len(final_points) == self.synthesis_max_pts:
+                break
 
         for ii in range(len(final_points)):
             v_new = self.draw_vel(final_points[ii])
             final_points.append(v_new)
+        return final_points
 
     def draw_point(self):
-        return [0, 0]
+        return self.dart_thrower.random_sample()
 
     def draw_vel(self, p):
+        # TODO: use self.gaussian_mixture
         return [0, 0]
 
     def check_pcf_error(self, points):
-        return True
+        return self.target_pcf.compatible(points)
 
 
 if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
     crowd_syn = CrowdSynthesizer()
 
     # TODO:
@@ -102,6 +122,30 @@ if __name__ == '__main__':
     dir = '/home/cyrus/workspace2/OpenTraj/ETH/seq_eth'
     parser.load(os.path.join(dir, 'obsmat.txt'))
     crowd_syn.extract_features(parser)
+
+    all_ts = parser.t_p_dict.keys()
+    t_begin, t_end = min(all_ts), max(all_ts)
+    all_ps = parser.get_all_points()
+    all_ps = np.array(all_ps)
+    x_min, x_max = min(all_ps[:, 0]), max(all_ps[:, 0])
+    y_min, y_max = min(all_ps[:, 1]), max(all_ps[:, 1])
+
+    for kk in range(100):
+        (rnd_t, gt_pts) = random.choice(list(parser.t_p_dict.items()))
+        gt_pts = np.array(gt_pts)
+        if len(gt_pts) < 4: continue
+        final_pnts = crowd_syn.synthesize_init(gt_pts[:3].tolist())
+        final_pnts = np.array(final_pnts)
+
+        plt.scatter(gt_pts[:, 0], gt_pts[:, 1], color='green', s=100)
+        plt.scatter(final_pnts[:, 0], final_pnts[:, 1], marker='o')
+        plt.xlim([x_min, x_max])
+        plt.ylim([y_min, y_max])
+        # plt.ion()
+        plt.show()
+        # plt.pause(10.0)
+
+        print('here')
 
 
 
--- src/crowd_prediction/crowd_synthesis/pcf.py
+++ src/crowd_prediction/crowd_synthesis/pcf.py
@@ -2,65 +2,108 @@ import os
 import numpy as np
 from math import sqrt
 from sklearn.metrics.pairwise import euclidean_distances
-
 # from followbot.temp.parser_eth import ParserETH
+from bisect import bisect_left, bisect_right
 
 
-def pcf(points, rng=np.arange(0.2, 10, 0.05), sigma=0.3):
-    N = len(points)
-    dists = euclidean_distances(points)
-    dists_wo_diag = dists[~np.eye(N, dtype=bool)].reshape(N, N-1)
-    # return dists
+class PcfPattern:
+    def __init__(self, sigma_=0.25, compat_thresh=0.1):
+        self.pcf_values = []
+        self.rad_values = []
+        self.get_pcf = []
+        self.sigma = sigma_
+        self.compat_threshold = compat_thresh
 
-    pcf_lut = dict()  # PCF look-up table
-    for rr in rng:
-        A_r = np.pi * (rr + 0.2) ** 2 - np.pi * max((rr - 0.2), 0) ** 2
+    def update(self, points, rng=np.arange(0.2, 10, 0.05)):
+        N = len(points)
 
-        dists_sqr = np.power(dists_wo_diag - rr, 2)
-        dists_exp = np.exp(-dists_sqr / (sigma ** 2)) / (sqrt(np.pi) * sigma)
-        pcf_r = np.sum(dists_exp) / N**2 / 2. /A_r # because each dist is repeated twice
+        self.rad_values = rng
+        self.pcf_values = np.empty(len(rng), dtype=np.float)
+        self.get_pcf = lambda r: self.pcf_values[bisect_right(self.rad_values, r)]
 
-        pcf_lut[rr] = pcf_r
+        dists = euclidean_distances(points)
+        dists_wo_diag = dists[~np.eye(N, dtype=bool)].reshape(N, N-1)
+        # return dists
 
-    return pcf_lut
+        for ii, rr in enumerate(rng):
+            A_r = np.pi * (rr + 0.2) ** 2 - np.pi * max((rr - 0.2), 0) ** 2
+
+            dists_sqr = np.power(dists_wo_diag - rr, 2)
+            dists_exp = np.exp(-dists_sqr / (self.sigma ** 2)) / (sqrt(np.pi) * self.sigma)
+            pcf_r = np.sum(dists_exp) / N**2 / 2. /A_r # because each dist is repeated twice
+
+            self.pcf_values[ii] = pcf_r
+
+    def compatible(self, points):
+        if len(self.pcf_values) == 0:
+            print('Error! First call update pcf')
+            return False
+
+        N = len(points)
+        dists = euclidean_distances(points)
+        dists_wo_diag = dists[~np.eye(N, dtype=bool)].reshape(N, N - 1)
+
+        for ii, rr in enumerate(self.rad_values):
+            A_r = np.pi * (rr + 0.2) ** 2 - np.pi * max((rr - 0.2), 0) ** 2
+
+            dists_sqr = np.power(dists_wo_diag - rr, 2)
+            dists_exp = np.exp(-dists_sqr / (self.sigma ** 2)) / (sqrt(np.pi) * self.sigma)
+            pcf_r = np.sum(dists_exp) / N ** 2 / 2. / A_r  # because each dist is repeated twice
+
+            if pcf_r > self.pcf_values[ii] + self.compat_threshold:
+                return False
+        return True
 
 
 # ===========================
 # === Synthesis Algorithm ===
 # ===========================
-def sample_pom(pdf, real_points):
-    return np.random.rand(2)
+class DartThrowing:
+    def __init__(self):
+        self.pdf_accum = []
+        self.map_to_world_coord = lambda : 0
 
+    def set_pdf_grid(self, pdf_grid, map_to_world_coord):
+        sum = pdf_grid.reshape(-1).sum()
+        self.pdf_accum = pdf_grid.reshape(-1).cumsum() / (sum + 1E-19)
+        self.map_to_world_coord = map_to_world_coord
 
-def dart_with_social_space(points, thresh=0.5):
-    for kk in range(100):  # number of tries
-        new_point = np.random.rand(2) * np.array([3.5, 7]) + np.array([0.25, 0.5])
-        if len(points) == 0:
-            return new_point
-        new_point_repeated = np.repeat(new_point.reshape((1, 2)), len(points), axis=0)
-        dists = np.linalg.norm(np.array(points) - new_point_repeated, axis=1)
-        if min(dists) > thresh:
-            return new_point
+    def random_sample(self):
+        rnd = np.random.rand()
+        index = bisect_left(self.pdf_accum, rnd)
+        x, y = self.map_to_world_coord(index)
+        return (x, y)
 
-    # FIXME: otherwise ignore the distance and return sth
-    return np.random.uniform(2) * np.array([3.5, 7]) + np.array([0.25, 0.5])
+    # TODO:
+    #  test functions
+    def dart_with_social_space(self, points, thresh=0.5):
+        for kk in range(100):  # number of tries
+            new_point = np.random.rand(2) * np.array([3.5, 7]) + np.array([0.25, 0.5])
+            if len(points) == 0:
+                return new_point
+            new_point_repeated = np.repeat(new_point.reshape((1, 2)), len(points), axis=0)
+            dists = np.linalg.norm(np.array(points) - new_point_repeated, axis=1)
+            if min(dists) > thresh:
+                return new_point
 
+        # FIXME: otherwise ignore the distance and return sth
+        return np.random.uniform(2) * np.array([3.5, 7]) + np.array([0.25, 0.5])
 
-def dart_throwing(n, target_pcf, prior_pom):
-    error_eps = 0.5
-    points = []
-    counter = 0
-    while len(points) < n:
-        new_pt = sample_pom(pdf=prior_pom)
-        points_tmp = [points[:], new_pt]
-        new_pcf_lut = pcf(points_tmp)
-        new_pcf_vals = np.array(sorted(new_pcf_lut.items()))[:, 1]
-        err = np.clip(new_pcf_vals - target_pcf, 0, 1000)  # skip values smaller than target values
-        max_err = max(err)
-        if max_err < error_eps:
-            points.append(new_pt)
+    def dart_throwing(self, n, target_pcf, prior_pom):
+        error_eps = 0.5
+        points = []
+        counter = 0
+        while len(points) < n:
+            new_pt = sample_pom(pdf_grid=prior_pom)
+            points_tmp = [points[:], new_pt]
+            new_pcf_lut = pcf(points_tmp)
+            new_pcf_vals = np.array(sorted(new_pcf_lut.items()))[:, 1]
+            err = np.clip(new_pcf_vals - target_pcf, 0, 1000)  # skip values smaller than target values
+            max_err = max(err)
+            if max_err < error_eps:
+                points.append(new_pt)
 
-    return points
+        return points
 
 
 def get_rand_n_agents(hist):
@@ -71,13 +114,10 @@ def get_rand_n_agents(hist):
             return ii
 
 
-
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     import cv2
-    crowd_by_click()
-
-
-
+    # crowd_by_click()
+    pcf_pattern = PcfPattern()
 
 
